9e8a64d695104d918268026b9692d9fe15f073c0
Marwan Ramzi Mohamed <maroroza1600@gmail.com>
2023-09-26 12:16:24 +0300

    Finished Code
    Simplified some unclear parts of code

--- predict.py
+++ predict.py
@@ -1,43 +1,21 @@
-#     Basic usage: python predict.py /path/to/image checkpoint
-#     Options:
-#         Return top KKK most likely classes: python predict.py input checkpoint --top_k 3
-#         Use a mapping of categories to real names: python predict.py input checkpoint --category_names cat_to_name.json
-#         Use GPU for inference: python predict.py input checkpoint --gpu
-
-# sample bash cmd: python predict.py './flowers/test/42/image_05696.jpg' '../saved_models/checkpoint.pth' 'cat_to_name.json'
+#sample to run predict.py: 
+#python predict.py 'flower_data/test/1/image_06743.jpg' './saved_checkpoints/vgg16' --cat_to_names 'cat_to_name.json' --topk 5 
 
 import argparse
 import torch
 import torchvision
 from torchvision.models.vgg import VGG16_Weights
-from torchvision.models.resnet import ResNet50_Weights
 from torchvision.models.alexnet import AlexNet_Weights
 from torchvision import transforms, datasets, models
 from PIL import Image
 import numpy as np
 import json
 
-# import argparse
-# import torch
-# import torchvision
-# from torch import nn, optim
-# from torchvision import transforms
-# from torchvision.datasets import ImageFolder
-# from torch.utils import data
-# from PIL import Image
-# import numpy as np
-# import os, random
-# import matplotlib
-# import matplotlib.pyplot as plt
-# import json
-
 
 def loading_the_checkpoint(path):
-    checkpoint = torch.load(path)
+    checkpoint = torch.load(path + "/checkpoint.pth")
     if checkpoint['structure'] == "vgg16":
         model = torchvision.models.vgg16(weights=VGG16_Weights.DEFAULT)
-    elif checkpoint['structure'] == "resnet50":
-        model = torchvision.models.resnet50(weights=ResNet50_Weights.DEFAULT)
     elif checkpoint['structure'] == "alexnet":
         model = torchvision.models.alexnet(weights=AlexNet_Weights.DEFAULT)
     
@@ -77,23 +55,25 @@ def process_image(image):
 def predict(image_path, model, topk, device, cat_to_name):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
     '''
+    
+    # TODO: Implement the code to predict the class from an image file
+    model.eval()
     img = process_image(image_path).numpy()
     img = torch.from_numpy(np.array([img])).float()
-    # image = image.unsqueeze(0)
 
     img = img.to(device)
-    model.eval()
-    with torch.no_grad():
-        logps = model.forward(img)
-        
-    probability = torch.exp(logps).data
-        
-    probability, top_classes = probability.topk(topk, dim=1)
-    
-    idx_to_flower = {v:cat_to_name[k] for k, v in model.class_to_idx.items()}
-    predicted_flowers_list = [idx_to_flower[i] for i in top_classes.tolist()[0]]
 
-    return probability.tolist()[0], predicted_flowers_list
+    with torch.no_grad():
+        output = model.forward(img)
+        
+    probability = torch.exp(output).data
+
+    probs, classes = probability.topk(topk)
+
+    probs = probs[0].tolist()
+    flowers_labels = [cat_to_name[str(i+1)] for i in classes.tolist()[0]]
+    
+    return probs, flowers_labels
 
 def print_predictions(args):
 
@@ -109,17 +89,15 @@ def print_predictions(args):
 
     model = model.to(device)
 
-    # print(model.class_to_index)
-
     with open(args.category_names_json_filepath, 'r') as f:
         cat_to_name = json.load(f)
 
     # predict image
-    top_ps, top_classes = predict(args.image_filepath, model, args.top_k, device, cat_to_name)
+    probs, classes = predict(args.image_filepath, model, args.top_k, device, cat_to_name)
 
     print("Predictions:")
     for i in range(args.top_k):
-          print("#{: <3} {: <25} Prob: {:.2f}%".format(i, top_classes[i], top_ps[i]*100))
+          print(f"#{i: <3} {classes[i]: <25} Prob: {probs[i]*100:.2f}%")
     
 if __name__ == '__main__':
     
@@ -127,15 +105,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # required arguments
-    parser.add_argument(dest='image_filepath', help="This is a image file that you want to classify")
-    parser.add_argument(dest='model_filepath', help="This is file path of a checkpoint file, including the extension")
+    parser.add_argument(dest='image_filepath', help="Image file path required to classify")
+    parser.add_argument(dest='model_filepath', help="Model file path of a saved checkpoint file")
 
     # optional arguments
-    parser.add_argument('--category_names_json_filepath', dest='category_names_json_filepath', help="This is a file path to a json file that maps categories to real names", default='cat_to_name.json')
-    parser.add_argument('--top_k', dest='top_k', help="This is the number of most likely classes to return, default is 5", default=5, type=int)
-    parser.add_argument('--gpu', dest='gpu', help="Include this argument if you want to train the model on the GPU via CUDA", action='store_true')
+    parser.add_argument('--cat_to_names', dest='cat_to_names', help="JSON file path that maps flower labels", default='cat_to_name.json')
+    parser.add_argument('--topk', dest='topk', help="This argument allows the user to know the top k flower categories classification, default is k=5", default=5, type=int)
+    parser.add_argument('--gpu', dest='gpu', help="This argument allows the user to train the model on the GPU via CUDA", action='store_true')
 
     # Parse and print the results
     args = parser.parse_args()
 
     print_predictions(args)
+
+    
--- train.py
+++ train.py
@@ -1,15 +1,7 @@
-# Basic usage: python train.py data_directory
-# Prints out training loss, validation loss, and validation accuracy as the network trains
-# Options:
-#         Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
-#         Choose architecture: python train.py data_dir --arch "vgg13"
-#         Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
-#         Use GPU for training: python train.py data_dir --gpu
-
-# sample bash cmd: python train.py './flowers' '../saved_models' --epochs 5
+#sample to run train.py: 
+#python train.py './flower_data' --save_directory './saved_checkpoints' --model_arch 'alexnet'
 
 import argparse
-import os
 
 import numpy as np
 import pandas as pd
@@ -20,44 +12,15 @@ from torchvision import transforms, datasets, models
 import torch
 from torch import nn, optim
 from torchvision.models.vgg import VGG16_Weights
-from torchvision.models.resnet import ResNet50_Weights, resnet50
 from torchvision.models.alexnet import AlexNet_Weights
 from PIL import Image
-from torch.autograd import Variable
-# import argparse
-# import torch
-# import torchvision
-# from torch import nn, optim
-# from torchvision import transforms
-# from torchvision.datasets import ImageFolder
-# from torch.utils import data
-# from PIL import Image
-# import numpy as np
-# import os, random
-# import matplotlib
-# import matplotlib.pyplot as plt
-# import json
-
 
 
 def data_transforms(args):
     
-    train_dir = os.path.join(args.data_directory, "train")
-    valid_dir = os.path.join(args.data_directory, "valid")
-
-    #Check for data directory
-    if not os.path.exists(train_dir):
-        print("Not valid Train folder: {}".format(train_dir))
-        raise FileNotFoundError
-    if not os.path.exists(valid_dir):
-        print("Not valid Valid folder: {}".format(valid_dir))
-        raise FileNotFoundError
-    #Check for checkpoint save directory 
-    if not os.path.exists(args.save_directory):
-        print("Not valid Save Directory: {}".format(args.save_directory))
-        raise FileNotFoundError
-
-
+    train_dir = args.data_directory + "/train"
+    valid_dir = args.data_directory + "/valid"
+    # print(train_dir)
 
     train_transforms = transforms.Compose([transforms.RandomRotation(degrees = 25),
                                            transforms.RandomResizedCrop(size = (224,224)),
@@ -87,9 +50,6 @@ def training(args, trainloader, validloader, class_to_idx):
     if args.model_arch == "vgg16":
         model = torchvision.models.vgg16(weights=VGG16_Weights.DEFAULT)
         features = model.classifier[0].in_features
-    elif args.model_arch == "resnet50":
-        model = torchvision.models.resnet50(weights=ResNet50_Weights.DEFAULT)
-        features = model.fc.in_features     #
     elif args.model_arch == "alexnet":
         model = torchvision.models.alexnet(weights=AlexNet_Weights.DEFAULT)
         features = model.classifier[1].in_features
@@ -99,7 +59,6 @@ def training(args, trainloader, validloader, class_to_idx):
         param.requires_grad = False
 
     #getting the dimensions of features extracted by the convolutional layers of the model
-    # features = model.classifier[0].in_features
     print(f"output of last layer in {args.model_arch}: {features}")  ##last layer output
 
     flower_categ = len(class_to_idx) #classifer output should be equal to the number of flower classes = 102
@@ -158,10 +117,8 @@ def training(args, trainloader, validloader, class_to_idx):
             if steps % print_every == 0 or steps == 1 or steps == len(trainloader):
                 print(f"Epoch: {i+1}/{epochs} Batch % Complete: {(steps)*100/len(trainloader):.2f}%")
 
-        # validate
-        # turn model to eval mode
-        # turn on no_grad
-
+        #validation
+        
         model.eval()
         valid_loss = 0
         accuracy = 0
@@ -176,19 +133,14 @@ def training(args, trainloader, validloader, class_to_idx):
                 
                 ps = torch.exp(outputs)
                 top_p, top_class = ps.topk(1, dim=1)
-                # print(f"top_class: {top_class}")
-                # print(f"top_p: {top_p}")
+                
                 equals = top_class == labels.view(*top_class.shape)
-                # print(f"labels.view(*top_class.shape): {labels.view(*top_class.shape)}")
-                # print(f"equals: {equals}")
-                # print(f"Info: {equals.type(torch.FloatTensor)}")
-                # print(f"labels.shape(): {labels.shape()}")
+                
                 #equals.type(torch.FloatTensor):
                 #Transforms "equals" the tensor of array of boolean to tensor of floating ones and zeros
                 #Then get the means of these numbers and finaly transforms the mean from tensor to python number
                 accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
         print(f"Epoch {i+1}/{epochs}.. "
-                # f"Loss: {running_loss/print_every:.3f}.. "
                 f"Train Loss: {running_loss/len(trainloader):.3f}.. "
                 f"Validation Loss: {valid_loss/len(validloader):.3f}.. "
                 f"Accuracy: {accuracy*100/len(validloader):.3f}%")
@@ -208,36 +160,34 @@ def training(args, trainloader, validloader, class_to_idx):
                     'class_to_idx': model.class_to_idx
                 }
 
-    torch.save(checkpoint, os.path.join(args.save_directory, "checkpoint.pth"))
-    save_dir = os.path.join(args.save_directory, "checkpoint.pth")
+    save_dir = args.save_directory + "/checkpoint.pth"
+    torch.save(checkpoint, save_dir)
     print(f"model saved to {save_dir}")
     return True
 
 if __name__ == '__main__':
 
-    # Create the parser and add arguments
+    # Creating the parser
     parser = argparse.ArgumentParser()
 
-    # required arguments
-    parser.add_argument(dest='data_directory', help="Directory of the train and valid images")
-    # parser.add_argument(dest='data_directory', help="This is the dir of the training images e.g. if a sample file is in /flowers/train/daisy/001.png then supply /flowers. Expect 2 folders within, 'train' & 'valid'")
-
-    # optional arguments
-    parser.add_argument('--save_directory', dest='save_directory', help="Directory of saved checkpoint.", default=r'C:\Users\Marwan\Desktop\aipnd-project-master')
-    parser.add_argument('--learning_rate', dest='learning_rate', default=0.001, type=float)
+    # Required arguments
+    parser.add_argument(dest='data_dir', help="Directory of the train and valid images")
+    
+    # Optional arguments
+    parser.add_argument('--save_dir', dest='save_dir', help="Directory of saved checkpoint.", default=r'C:\Users\Marwan\Desktop\aipnd-project-master')
+    parser.add_argument('--lr', dest='lr', default=0.001, type=float)
     parser.add_argument('--epochs', dest='epochs', default=3, type=int)
     parser.add_argument('--gpu', dest='gpu', help="This argument allows the user to train the model on the GPU via CUDA", action='store_true')
-    parser.add_argument('--model_arch', dest='model_arch', help="This argument allows the user to choose from 3 different pretrained models.", default="vgg16", type=str, choices=['vgg16', 'resnet50', 'alexnet'])
+    parser.add_argument('--model', dest='model', help="This argument allows the user to choose from 2 different pretrained models.", default="vgg16", type=str, choices=['vgg16', 'alexnet'])
     parser.add_argument('--hidden_units', action="store", dest="hidden_units", type=int, default=2048)
     parser.add_argument('--dropout', action="store", type=float, default=0.25)
 
-    # Parse and print the results
     args = parser.parse_args()
 
-    # load and transform data
     trainloader, validloader, class_to_idx = data_transforms(args)
 
-    # train and save model
     training(args, trainloader, validloader, class_to_idx)
+
+
     
     
